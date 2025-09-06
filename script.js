0c582067b743b6721612386bfe4c133a943b7787
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-09-06 16:58:40 +0300

    Add navbar with animated logo, styles links, and hover effect

---
+++ script.js
@@ -0,0 +1,16 @@
+// =======================
+// Logo Hover Effect Script
+// Rotates the logo slightly on mouse enter
+// Resets rotation on mouse leave
+// =======================
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('mouseenter', () =>{
+    logo.style.transition = "transform 0.6s ease";
+    logo.style.transform = "rotate(-15deg)";
+});
+
+logo.addEventListener('mouseleave', () =>{
+    logo.style.transition = "transform 0.6s ease";
+    logo.style.transform = "rotate(0deg)";
+});
