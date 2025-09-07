cac5d3ddd558b3456bccad88e3982605f7c62824
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-09-07 17:00:29 +0300

    Add spacing on elements in nav bar, staggered slide-in, and smooth bounce animation

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 // Rotates the logo slightly on mouse enter
 // Resets rotation on mouse leave
 // =======================
-const logo = document.querySelector('.logo');
+const logo = document.querySelector('.logo')
 
 logo.addEventListener('mouseenter', () =>{
     logo.style.transition = "transform 0.6s ease";
-    logo.style.transform = "rotate(-15deg)";
+    logo.style.transform = "rotate(-12deg)";
 });
 
 logo.addEventListener('mouseleave', () =>{
